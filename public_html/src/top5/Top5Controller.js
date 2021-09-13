6875c9805b10c025a1d0c87af69a3ec81f1834a4
Noah <nochen@dep.nyc.gov>
2021-09-13 16:57:30 -0400

    Added example situation from last commit, fixed (visual) bugs from all toolbar buttons, need to add blur events to item and list name changes

--- public_html/src/top5/Top5Controller.js
+++ public_html/src/top5/Top5Controller.js
@@ -22,6 +22,7 @@ export default class Top5Controller {
             let newList = this.model.addNewList("Untitled", ["?","?","?","?","?"]);            
             this.model.loadList(newList.id);
             this.model.saveLists();
+            document.getElementById("add-list-button").disabled = true;
         }
         document.getElementById("undo-button").onmousedown = (event) => {
             this.model.undo();
@@ -56,6 +57,7 @@ export default class Top5Controller {
                     textInput.onkeydown = (event) => {
                         if (event.key === 'Enter') {
                             this.model.addChangeItemTransaction(i-1, event.target.value);
+                            document.getElementById("undo-button").classList.remove("disabled");
                         }
                     }
                     textInput.onblur = (event) => {
@@ -64,26 +66,17 @@ export default class Top5Controller {
                 }
             }
             item.ondragstart = (ev) => {
-                //console.log(this.model.currentList.items.indexOf(ev.target.innerHTML));
-                //console.log(ev.target.innerHTML);
-                //let oldIndex = this.model.currentList.items.indexOf(ev.target.innerHTML);
                 ev.dataTransfer.setData("Text", ev.target.innerHTML)
             }
             item.ondragover = (ev) => {
                 ev.preventDefault();
             }
             item.ondrop = (ev) => {
-                //console.log(ev.target.id);
-                //console.log(this.model.currentList.items.indexOf(ev.target.innerHTML));
                 let newIndex = this.model.currentList.items.indexOf(ev.target.innerHTML);
                 let oldIndex = this.model.currentList.items.indexOf(ev.dataTransfer.getData("Text"));
-                //console.log(oldIndex);
-                //console.log(newIndex);
-                //this.model.currentList.moveItem(oldIndex, newIndex);
                 this.model.addMoveItemTransaction(oldIndex, newIndex);
-                //this.model.loadList(this.model.currentList.id);
-                //console.log(this.model.currentList);
                 this.model.saveLists();
+                document.getElementById("undo-button").classList.remove("disabled");
             }
         }
     }
@@ -104,10 +97,11 @@ export default class Top5Controller {
         }
         //FOR CHANGING THE LIST NAME
         document.getElementById("top5-list-" + id).ondblclick = (event) => {
-            //console.log("a")
             document.getElementById("list-card-text-" + id).readOnly = false;
             let listModel = this.model;
             let clickedList = document.getElementById("top5-list-" + id);
+            clickedList.children[0].style.backgroundColor = "white";
+            clickedList.children[0].style.color = "black";
             clickedList.children[0].addEventListener('keyup', function(event) {
                 event.preventDefault();
                 if (event.key === "Enter") {
@@ -117,6 +111,7 @@ export default class Top5Controller {
                     document.getElementById("top5-statusbar").children[0].innerHTML = 
                         listModel.getList(listModel.getListIndex(id)).getName();
                     document.getElementById("list-card-text-" + id).readOnly = true;
+                    clickedList.children[0].style.backgroundColor = "transparent";
                 }
             })
         }
@@ -130,24 +125,27 @@ export default class Top5Controller {
             let deleteSpan = document.getElementById("delete-list-span");
             deleteSpan.innerHTML = "";
             deleteSpan.appendChild(document.createTextNode(listName));
+            let deletemodel = this.model;
             modal.classList.add("is-visible");
             document.getElementById("dialog-confirm-button").onmousedown = (event) => {
-                //console.log("b");
                 modal.classList.remove("is-visible");
+                if (document.getElementById("top5-list-" + id).classList.contains("selected-list-card")) {
+                    deletemodel.unselectAll();
+                    document.getElementById("top5-statusbar").children[0].innerHTML = "";
+                    deletemodel.view.clearWorkspace();
+                    document.getElementById("add-list-button").disabled = false;
+                }
                 this.model.deleteList(id);
                 this.model.saveLists();
             }
             document.getElementById("dialog-cancel-button").onmousedown = (event) => {
-                //console.log("c");
                 modal.classList.remove("is-visible");
             }
         }
         document.getElementById("close-button").onmousedown = (event) => {
-            //console.log("d");
             this.model.unselectAll();
             document.getElementById("top5-statusbar").children[0].innerHTML = "";
             this.model.view.clearWorkspace();
-
             document.getElementById("add-list-button").disabled = false;
         }
     }
